5dbe3f0fc88275bd37d815d232bdddc1208867a7
Hamza HRAMCHI <hramchi.hamza@gmail.com>
2020-11-10 22:10:00 +0100

    Third exercice : roll dice by EditText

--- TD3_lance_de/TD3/app/src/main/java/com/example/td3/ThirdExercice.java
+++ TD3_lance_de/TD3/app/src/main/java/com/example/td3/ThirdExercice.java
@@ -1,6 +1,32 @@
 package com.example.td3;
 
+import android.os.Bundle;
+import android.text.Editable;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ThirdExercice extends AppCompatActivity {
+    TextView result;
+    EditText nombre;
+    Button btn_lancer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex03);
+
+        result = findViewById(R.id.result);
+        nombre = findViewById(R.id.nombre);
+        btn_lancer = findViewById(R.id.btn);
+
+        btn_lancer.setOnClickListener(v -> {
+            Toast.makeText(ThirdExercice.this, "Dé lancé !", Toast.LENGTH_LONG).show();
+            int nombre =  (int) (Math.random() * Integer.parseInt(this.nombre.toString())) + 1;
+            this.result.setText(String.valueOf(nombre));
+        });
+    }
 }
