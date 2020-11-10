a235c2359e9b2e467b1cb61541c028edc4112425
Hamza HRAMCHI <hramchi.hamza@gmail.com>
2020-11-10 22:29:27 +0100

    Third exercice : roll dice by EditText value

--- TD3_lance_de/TD3/app/src/main/java/com/example/td3/ThirdExercice.java
+++ TD3_lance_de/TD3/app/src/main/java/com/example/td3/ThirdExercice.java
@@ -10,7 +10,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ThirdExercice extends AppCompatActivity {
-    TextView result;
+    TextView result1;
+    TextView result2;
     EditText number;
     Button btn_lancer;
 
@@ -19,19 +20,25 @@ public class ThirdExercice extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ex03);
 
-        result = findViewById(R.id.result);
+        result1 = findViewById(R.id.result1);
+        result2 = findViewById(R.id.result2);
+
         number = findViewById(R.id.nombre);
         btn_lancer = findViewById(R.id.btn);
 
         btn_lancer.setOnClickListener(v -> {
             // Toast.makeText(ThirdExercice.this, "Dé lancé !", Toast.LENGTH_LONG).show();
-            int value;
+            int value1;
+            int value2;
             if (this.number.getText().toString().equals("")) {
-                 value =  (int) (Math.random() * 10) + 1;
+                 value1 =  (int) (Math.random() * 0) + 1;
+                 value2 =  (int) (Math.random() * 1) + 1;
             } else {
-                 value =  (int) (Math.random() * Integer.parseInt(this.number.getText().toString())) + 1;
+                 value1 =  (int) (Math.random() * Integer.parseInt(this.number.getText().toString())) + 1;
+                 value2 =  (int) (Math.random() * Integer.parseInt(this.number.getText().toString())) + 2;
             }
-            this.result.setText(String.valueOf(value));
+            this.result1.setText(String.valueOf(value1));
+            this.result2.setText(String.valueOf(value2));
         });
     }
 }
