c7110483d1de4f78eca6abb033b84b6f6843feec
hamza-hramchi <hramchi.hamza@gmail.com>
2020-11-24 19:43:32 +0100

    Crate contact class + item_contact xml file

---
+++ TD5_les_listes/app/src/main/java/com/example/td5/Contact.java
@@ -0,0 +1,12 @@
+package com.example.td5;
+
+public class Contact {
+    public String nom;
+    public String prenom;
+    public String imageUrl;
+
+    public Contact(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
