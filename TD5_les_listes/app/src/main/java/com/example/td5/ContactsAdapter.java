4d3c90970b18d46d4b72f5aca1bb2e866ce71ad8
hamza-hramchi <hramchi.hamza@gmail.com>
2020-11-24 19:54:48 +0100

    Create RecyclerView adapter + link adaptater to RecyclerView

--- TD5_les_listes/app/src/main/java/com/example/td5/ContactsAdapter.java
+++ TD5_les_listes/app/src/main/java/com/example/td5/ContactsAdapter.java
@@ -1,4 +1,55 @@
 package com.example.td5;
 
-public class ContactsAdapter {
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
+    public final List<Contact> contacts;
+    public ContactsAdapter(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+        return new ViewHolder(contactView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Contact contact = contacts.get(position);
+        TextView firstName = holder.firstName;
+        firstName.setText(contact.nom);
+        TextView lastName = holder.lastName;
+        lastName.setText(contact.prenom);
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView firstName;
+        public TextView lastName;
+        public ImageView image;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            firstName = itemView.findViewById(R.id.firstName);
+            lastName =  itemView.findViewById(R.id.lastName);
+            image =     itemView.findViewById(R.id.image);
+        }
+    }
 }
--- TD5_les_listes/app/src/main/java/com/example/td5/UserActivity.java
+++ TD5_les_listes/app/src/main/java/com/example/td5/UserActivity.java
@@ -3,12 +3,31 @@ package com.example.td5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserActivity extends AppCompatActivity {
+    List<Contact> contacts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialisation
+        contacts.add(new Contact("HRAMCHI", "Hamza"));
+        contacts.add(new Contact("MOUSAOUI", "Yahya"));
+        contacts.add(new Contact("KHOUYA", "Ayoub"));
+        contacts.add(new Contact("ATABIT", "Meryeme"));
+        contacts.add(new Contact("EL HADRI", "Farah"));
+
+        // Create adapter + initialisation
+        ContactsAdapter adapter = new ContactsAdapter(contacts);
+        rvContacts.setAdapter(adapter);
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
