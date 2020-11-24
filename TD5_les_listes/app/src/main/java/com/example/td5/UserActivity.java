5ca6a159d3501d452416f6e7de39f85b9aaefd42
hamza-hramchi <hramchi.hamza@gmail.com>
2020-11-24 21:20:44 +0100

    Update image urls

--- TD5_les_listes/app/src/main/java/com/example/td5/UserActivity.java
+++ TD5_les_listes/app/src/main/java/com/example/td5/UserActivity.java
@@ -18,11 +18,11 @@ public class UserActivity extends AppCompatActivity {
         RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
 
         // Initialisation
-        contacts.add(new Contact("HRAMCHI", "Hamza", "https://histoire-image.org/sites/default/jeanne-arc-sacre-charlesvii.jpg"));
-        contacts.add(new Contact("MOUSAOUI", "Yahya", "https://histoire-image.org/sites/default/jeanne-arc-sacre-charlesvii.jpg"));
-        contacts.add(new Contact("KHOUYA", "Ayoub", "https://histoire-image.org/sites/default/jeanne-arc-sacre-charlesvii.jpg"));
-        contacts.add(new Contact("ATABIT", "Meryeme", "https://histoire-image.org/sites/default/jeanne-arc-sacre-charlesvii.jpg"));
-        contacts.add(new Contact("EL HADRI", "Farah", "https://histoire-image.org/sites/default/jeanne-arc-sacre-charlesvii.jpg"));
+        contacts.add(new Contact("HRAMCHI", "Hamza", "https://cdn.iconscout.com/icon/free/png-256/developer-2309906-1943816.png"));
+        contacts.add(new Contact("MOUSAOUI", "Yahya", "https://cdn3.iconfinder.com/data/icons/software-development-flat-colorful/2048/5438_-_Software_Developer-512.png"));
+        contacts.add(new Contact("KHOUYA", "Ayoub", "https://cdn0.iconfinder.com/data/icons/search-engine-optimization-29/66/5-512.png"));
+        contacts.add(new Contact("ATABIT", "Meryeme", "https://img.flaticon.com/icons/png/512/1508/1508843.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF"));
+        contacts.add(new Contact("EL HADRI", "Farah", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMeXbmn2_9-Y6lvkDJ_XuDVkNdcHj1OTAzQg&usqp=CAU"));
 
         // Create adapter + initialisation
         ContactsAdapter adapter = new ContactsAdapter(contacts);
